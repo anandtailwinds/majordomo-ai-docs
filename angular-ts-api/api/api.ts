aa53e14e22ad55074391e50e28d52151dc9bab6c
Anand Natarajan <anand@tailwinds.ai>
2024-12-18 11:17:10 +0000

    Angular files removed.

--- angular-ts-api/api/api.ts
+++
@@ -1,9 +0,0 @@
-export * from './admin.service';
-import { AdminService } from './admin.service';
-export * from './ingest.service';
-import { IngestService } from './ingest.service';
-export * from './monitor.service';
-import { MonitorService } from './monitor.service';
-export * from './query.service';
-import { QueryService } from './query.service';
-export const APIS = [AdminService, IngestService, MonitorService, QueryService];
